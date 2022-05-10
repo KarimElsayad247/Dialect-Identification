37867d181fbd9667940a834fdf1e3c758a84e53a
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-05-10 15:35:32 +0200

    Try to finetune marbert model
    Keeps failing, for some reason, the huggingface model is
    trying to run on the gpu, despite not being told to do so
    anywhere.

--- QadiDataset.py
+++ QadiDataset.py
@@ -1,24 +1,30 @@
 import os
-from typing import Tuple
+from typing import Tuple, Dict
 import pandas as pd
+import torch
 from torch.utils.data import Dataset
+from transformers import PreTrainedTokenizer
 
 
 class QadiDataset(Dataset):
 
-    def __init__(self, data_file_name: str, qadi_data_folder: str):
+    def __init__(self, data_file_name: str, qadi_data_folder: str, tokenizer: PreTrainedTokenizer):
         """
         Load data from specified file and put them in memory.
         Assumes the name of the column containing index/id of a tweet is "id".
 
         :param: data_file_name: name of file containing data
         :param: qadi_data_folder: path of folder containing the file
+        :param: tokenizer: huggingface AutoTokenizer to use in tokenizing dataset text
         """
         self.data: pd.DataFrame = pd.read_csv(
             os.path.join(qadi_data_folder, data_file_name),
             sep='\t',
             lineterminator='\n',
             index_col='id')
+        self.encodings = tokenizer(self.data["text"].tolist(), padding=True)
+        self.labels = self.data["country_label"]
+        self.label_nums = {country: i for i, country in enumerate(self.labels.unique())}
 
     def __len__(self):
         """
@@ -26,24 +32,24 @@ class QadiDataset(Dataset):
         """
         return self.data.shape[0]
 
-    def __getitem__(self, item: int) -> Tuple[str, str]:
+    def __getitem__(self, idx: int) -> Dict:
         """
-        Returns a tuple consisting of the tweet content and country label
-        :param: item: index of the sample to return
-        :return: sample at index item in the dataset, along with its label
+        :param: idx: index of the sample to return
+        :return: encoding of the tweet, and its label
         """
-        text = self.data.iloc[item]["text"]
-        label = self.data.iloc[item]["country_label"]
-
-        return text, label
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item["labels"] = torch.tensor(self.label_nums[self.labels[idx]])
+        return item
 
 
 if __name__ == "__main__":
 
-    data = QadiDataset("dev_set.tsv", "Data/QADI")
+    import transformers
+
+    data = QadiDataset("dev_set.tsv", "Data/QADI", transformers.AutoTokenizer.from_pretrained("UBC-NLP/MARBERT"))
     print(f"There are {len(data)} samples")
-    text, label = data[0]
-    print(f"tweet '{text}' is from {label}")
+    sample = data[0]
+    print(sample)
 
 
 
--- main.py
+++ main.py
@@ -1,27 +1,71 @@
+import gc
+
+import numpy as np
+import torch
 import transformers
 
 from QadiDataset import QadiDataset
 import utils
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoModel, AutoModelForSequenceClassification, AutoTokenizer
+from transformers import Trainer, TrainingArguments
+from datasets import load_metric, Metric
 from torch.utils.data import DataLoader
 
 
-def train(train_data):
+
+accuracy_metric = load_metric("accuracy")
+
+
+# see https://huggingface.co/docs/transformers/training#metrics
+def compute_metric_accuracy(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return accuracy_metric.compute(predictions=predictions, references=labels)
+
+
+# see https://huggingface.co/docs/transformers/training#train
+def train(configs):
+
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    device = torch.device('cpu')
 
     tokenizer: transformers.PreTrainedTokenizer = AutoTokenizer.from_pretrained("UBC-NLP/MARBERT")
-    model = AutoModel.from_pretrained("UBC-NLP/MARBERT")
+    model: AutoModelForSequenceClassification = AutoModelForSequenceClassification.from_pretrained("UBC-NLP/MARBERT", num_labels=18)
+    model.to(device)
 
-    train_loader = DataLoader(dataset=train_data, batch_size=64, shuffle=True)
+    dev_data = QadiDataset(configs["dev_file_name"], configs["data_files_path"], tokenizer)
+    train_data = QadiDataset(configs["dev_file_name"], configs["data_files_path"], tokenizer)
+
+
+    # train_loader = DataLoader(dataset=train_data, batch_size=64, shuffle=True)
 
     num_epochs = 1
 
-    # both features and labels are tuples
-    for features, labels in train_loader:
-        print(len(features), len(labels))
+    training_arguments: TrainingArguments = TrainingArguments(
+        output_dir="test_trainer",
+        num_train_epochs=num_epochs,
+        per_device_train_batch_size=16,  # batch size per device during training
+        per_device_eval_batch_size=64,   # batch size per device during evaluation
+        evaluation_strategy=transformers.IntervalStrategy.EPOCH)
+
+
+    trainer: Trainer = Trainer(
+        model=model,
+        args=training_arguments,
+        train_dataset=train_data,
+        eval_dataset=dev_data,
+        compute_metrics=compute_metric_accuracy
+    )
+
+    trainer.train()
+    # # both features and labels are tuples
+    # for features, labels in train_loader:
+    #     print(len(features), len(labels))
 
 
 if __name__ == '__main__':
 
     configs = utils.read_yaml_file("config.yaml")
-    dev_data = QadiDataset(configs["dev_file_name"], configs["data_files_path"])
-    train(dev_data)
+    train(configs)
